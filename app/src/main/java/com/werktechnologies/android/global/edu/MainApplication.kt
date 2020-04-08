0b51ce1c132b5b6e4928a9e805300c3c3a8fb7b4
amlwin <amlwin.mm@gmail.com>
2020-04-08 14:28:49 +0630

    initial 🚀

---
+++ app/src/main/java/com/werktechnologies/android/global/edu/MainApplication.kt
@@ -0,0 +1,14 @@
+package com.werktechnologies.android.global.edu
+
+import android.app.Application
+
+/**
+ * a touch of amlwin on 08/04/2020.
+ * made with <3
+ */
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
